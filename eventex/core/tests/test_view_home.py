1f55cfba552b0714cbeb0afeaea132be282ffa93
Tiago Almeida <tyagow@hotmail.com.br>
2016-11-16 08:54:00 -0200

    Speaker adicionado

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -3,6 +3,8 @@ from django.shortcuts import resolve_url as r
 
 
 class HomePageTest(TestCase):
+    fixtures = ['keynotes.json']
+
     def setUp(self):
         self.response = self.client.get(r('home'))
 
@@ -16,6 +18,24 @@ class HomePageTest(TestCase):
 
     def test_subscription_link(self):
         expected = 'href="{}"'.format(r('subscriptions:new'))
+        self.assertContains(self.response, expected)
+
+    def test_speakers(self):
+        """Must show keynote speakers"""
+        contents = [
+            'href="{}"'.format(r('speaker_detail', slug='grace-hopper')),
+            'Grace Hopper',
+            'http://hbn.link/hopper-pic',
+            'href="{}"'.format(r('speaker_detail', slug='alan-turing')),
+            'Alan Turing',
+            'http://hbn.link/turing-pic',
+        ]
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(self.response, expected)
+
+    def test_speakers_link(self):
+        expected = 'href="{}#speakers"'.format(r('home'))
         self.assertContains(self.response, expected)
 
 
--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from eventex.core.models import Speaker
 
 
 def home(request):
-    return render(request, 'index.html')
+    speakers = Speaker.objects.all()
+    return render(request, 'index.html', {'speakers': speakers})
+
+
+def speaker_detail(request, slug):
+    speaker = get_object_or_404(Speaker, slug=slug)
+    return render(request, 'core/speaker_detail.html', {'speaker': speaker})
